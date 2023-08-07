af012cd9223ba6154f8101fa4f9c386df6164e9a
fcb1014 <fcb1014@hanyang.ac.kr>
2023-08-07 17:48:02 +0900

    230807 수업자료 업데이트2

--- JavaTest/src/com/test/java/Ex34_String.java
+++ JavaTest/src/com/test/java/Ex34_String.java
@@ -38,7 +38,7 @@ public class Ex34_String {
 		System.out.println(Arrays.toString(names));
 		
 		for (int i=0; i<names.length; i++) {
-//			System.out.println(names[i]);
+			System.out.println(names[i]);
 			System.out.println(names.length);
 		}
 		
--- JavaTest/src/com/test/question/directoryAccess/Q110.java
+++ JavaTest/src/com/test/question/directoryAccess/Q110.java
@@ -9,19 +9,22 @@ public class Q110 {
 		String path = "";
 		String fileName = "";
 		String extension = "";
-		int fileSize = 0;
+		double fileSize = 0;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("파일 경로: ");
-		path = scan.nextLine();
+//		System.out.print("파일 경로: ");
+//		path = scan.nextLine();
+		path = "C:\\class\\code\\java\\file\\hello.txt";
 		
 		File file = new File(path);
 		
 		fileName = file.getName();
-		extension = fileName.split(".")[1];
+		extension = fileName.split(".")[0];
+		fileSize = file.length();
 		
-		
-		
+		System.out.println("파일명: " + fileName);
+		System.out.println("종류: " + extension + " 파일");
+		System.out.println("파일 크기: " + fileSize);
 	}
 }
